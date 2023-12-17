b12a8fc11ee43027d187bc64aa0816cbbcdde7fc
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2023-12-17 14:25:03 +0100

    add lines for running new configuration

--- src/scripts/run_trauma_stai.sh
+++ src/scripts/run_trauma_stai.sh
@@ -15,6 +15,17 @@ module load anaconda/3/2021.11
 pip3 install --user openai ipdb python-dotenv tqdm
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
-python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123
-python query.py --llm gpt4 --condition trauma_stai --prompt-length brief --seed 123
-python query.py --llm gpt4 --condition trauma_relaxation_stai --prompt-length brief --seed 123
+# run 0: system fingerprint "fp_a24b4d720c"
+# python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123
+# python query.py --llm gpt4 --condition trauma_stai --prompt-length brief --seed 123
+# python query.py --llm gpt4 --condition trauma_relaxation_stai --prompt-length brief --seed 123
+
+# run 1
+python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123 --proc-id 1
+python query.py --llm gpt4 --condition trauma_stai --prompt-length brief --seed 123 --proc-id 1
+python query.py --llm gpt4 --condition trauma_relaxation_stai --prompt-length brief --seed 123 --proc-id 1
+python query.py --llm gpt4 --condition relaxation_stai --prompt-length brief --seed 123 --proc-id 1
+python query.py --llm gpt4 --condition relaxation_trauma_stai --prompt-length brief --seed 123 --proc-id 1
+
+
+
