921715ad0919c2458ad99aa5a367aac73ed3e954
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-05-17 13:24:46 +0200

    keep only the last used script

--- src/scripts/run_trauma_stai.sh
+++ src/scripts/run_trauma_stai.sh
@@ -15,21 +15,6 @@ module load anaconda/3/2021.11
 pip3 install --user openai ipdb python-dotenv tqdm
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
-# run 0: system fingerprint "fp_a24b4d720c"
-# python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123
-# python query.py --llm gpt4 --condition trauma_stai --prompt-length brief --seed 123
-# python query.py --llm gpt4 --condition trauma_relaxation_stai --prompt-length brief --seed 123
-
-# run 1
-# python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123 --proc-id 1
-# python query.py --llm gpt4 --condition trauma_stai --prompt-length brief --seed 123 --proc-id 1 --num-runs 5
-# python query.py --llm gpt4 --condition trauma_relaxation_stai --prompt-length brief --seed 123 --proc-id 1
-# python query.py --llm gpt4 --condition relaxation_stai --prompt-length brief --seed 123 --proc-id 1
-# python query.py --llm gpt4 --condition relaxation_trauma_stai --prompt-length brief --seed 123 --proc-id 1
-
-# run 2
-# python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123 --proc-id 2 --num-runs 5
-
 # run 3
 python query.py --llm gpt4 --condition stai --prompt-length brief --seed 123 --proc-id 3 --num-runs 5
 python query.py --llm gpt4 --condition trauma_stai --prompt-length brief --seed 123 --proc-id 3 --num-runs 5
